e42a0824482ec14889e01a1741d014a815696f39
NatalyOsadcha <114231206+NatalyOsadcha@users.noreply.github.com>
2023-07-11 16:55:19 +0300

    Added photo delete

--- Screens/CreatePostsScreen.jsx
+++ Screens/CreatePostsScreen.jsx
@@ -1,5 +1,9 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useNavigation, useRoute, useIsFocused } from "@react-navigation/native";
+import {
+  useNavigation,
+  useRoute,
+  useIsFocused,
+} from "@react-navigation/native";
 import { Camera } from "expo-camera";
 import * as MediaLibrary from "expo-media-library";
 import { FontAwesome, FontAwesome5, MaterialIcons } from "@expo/vector-icons";
@@ -13,6 +17,7 @@ import {
   TextInput,
   Keyboard,
   KeyboardAvoidingView,
+  Image,
 } from "react-native";
 import ActiveSubmitButton from "../Components/ActiveSubmitButton";
 import InactiveSubmitButton from "../Components/InactiveSubmitButton";
@@ -20,8 +25,9 @@ import InactiveSubmitButton from "../Components/InactiveSubmitButton";
 export default function CreatePostsScreen() {
   const [name, setName] = useState("");
   const [place, setPlace] = useState("");
+  const [photoUri, setPhotoUri] = useState(null);
   const navigation = useNavigation();
-  const { } = useRoute();
+  const {} = useRoute();
   const focused = useIsFocused();
 
   const [hasPermission, setHasPermission] = useState(null);
@@ -44,48 +50,64 @@ export default function CreatePostsScreen() {
     return <Text>No access to camera</Text>;
   }
 
+  const takePicture = async () => {
+    if (cameraRef) {
+      const { uri } = await cameraRef.takePictureAsync();
+      await MediaLibrary.createAssetAsync(uri);
+      setPhotoUri(uri);
+    }
+  };
+
+  // const deletePhoto = async () => {
+  //   if (photoUri) {
+  //     await MediaLibrary.deleteAssetsAsync([photoUri]);
+  //     setPhotoUri(null);
+  //   }
+  // };
+
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
       <View style={styles.container}>
         <KeyboardAvoidingView behavior="position">
           {hasPermission && focused && (
-            <Camera style={styles.camera} type={type} ref={setCameraRef}>
-              <View style={styles.photoView}>
-                <TouchableOpacity
-                  style={styles.flipContainer}
-                  onPress={() => {
-                    setType(
-                      type === Camera.Constants.Type.back
-                        ? Camera.Constants.Type.front
-                        : Camera.Constants.Type.back
-                    );
-                  }}
-                >
-                  <MaterialIcons
-                    name="flip-camera-android"
-                    size={35}
-                    color="#BDBDBD"
-                  />
-                </TouchableOpacity>
-                <TouchableOpacity
-                  style={styles.button}
-                  onPress={async () => {
-                    if (cameraRef) {
-                      const { uri } = await cameraRef.takePictureAsync();
-                      await MediaLibrary.createAssetAsync(uri);
-                    }
-                  }}
-                >
-                  <View style={styles.cameraWrapper}>
-                    <FontAwesome
-                      name="camera"
-                      size={24}
-                      color={"#BDBDBD"}
-                    />
+            <View style={styles.cameraView}>
+              {photoUri ? (
+                <Image style={styles.photo} source={{ uri: photoUri }} />
+              ) : (
+                <Camera style={styles.camera} type={type} ref={setCameraRef}>
+                  <View style={styles.photoView}>
+                    <TouchableOpacity
+                      style={styles.flipWrapper}
+                      onPress={() => {
+                        setType(
+                          type === Camera.Constants.Type.back
+                            ? Camera.Constants.Type.front
+                            : Camera.Constants.Type.back
+                        );
+                      }}
+                    >
+                      <MaterialIcons
+                        name="flip-camera-android"
+                        size={35}
+                        color="#BDBDBD"
+                      />
+                    </TouchableOpacity>
+                    <TouchableOpacity
+                      style={styles.button}
+                      onPress={takePicture}
+                    >
+                      <View style={styles.iconWrapper}>
+                        <FontAwesome
+                          name="camera"
+                          size={24}
+                          color={"#BDBDBD"}
+                        />
+                      </View>
+                    </TouchableOpacity>
                   </View>
-                </TouchableOpacity>
-              </View>
-            </Camera>
+                </Camera>
+              )}
+            </View>
           )}
           <Text style={styles.photoText}>Upload photo/ Edit photo</Text>
           <TextInput
@@ -111,21 +133,32 @@ export default function CreatePostsScreen() {
               <FontAwesome5 name="map-marker-alt" size={24} color="#BDBDBD" />
             </Pressable>
           </View>
-          {name !== "" ? (
-            <ActiveSubmitButton text={"Publish"} onPress={() => navigation.navigate("Posts")} />
+          {name !== "" && place !== "" && photoUri !== "null" ? (
+            <ActiveSubmitButton
+              text={"Publish"}
+              onPress={() => navigation.navigate("Posts")}
+            />
           ) : (
             <InactiveSubmitButton text={"Publish"} />
           )}
         </KeyboardAvoidingView>
         <View style={{ flex: 1 }}></View>
-        <Pressable style={styles.trashWrapper}>
-          <FontAwesome5 name="trash-alt" size={24} color="#BDBDBD" />
-        </Pressable>
+        {photoUri ? (
+          <Pressable
+            style={{ ...styles.trashWrapper, backgroundColor: "#FF6C00" }}
+            onPress={() => setPhotoUri(null)}
+          >
+            <FontAwesome5 name="trash-alt" size={24} color="#fff" />
+          </Pressable>
+        ) : (
+          <View style={{ ...styles.trashWrapper, backgroundColor: "#F6F6F6" }}>
+            <FontAwesome5 name="trash-alt" size={24} color="#BDBDBD" />
+          </View>
+        )}
       </View>
     </TouchableWithoutFeedback>
   );
 }
-
 
 const styles = StyleSheet.create({
   container: {
@@ -137,7 +170,7 @@ const styles = StyleSheet.create({
     borderStyle: "solid",
     padding: 16,
   },
-  cameraWrapper: {
+  button: {
     position: "absolute",
     top: "50%",
     left: "50%",
@@ -149,19 +182,33 @@ const styles = StyleSheet.create({
     width: 60,
     height: 60,
   },
-  photoView: {
-    height: 320,
-    overflow: "hidden",
-    backgroundColor: "transparent",
-    justifyContent: "center",
+  photo: {
+    height: 300,
+    borderRadius: 8,
   },
-  flipContainer: {
+  photoView: {
+    height: 300,
+    borderWidth: 1,
+    borderColor: "#E8E8E8",
+    borderRadius: 8,
+    position: "relative",
+  },
+  flipWrapper: {
     position: "absolute",
     bottom: 15,
     right: 15,
   },
+  cameraWrapper: {
+    left: "50%",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "rgba(255,255,255,0.4)",
+    borderRadius: 50,
+    width: 60,
+    height: 60,
+  },
   photoText: {
-    marginBottom:16,
+    marginBottom: 16,
     fontSize: 16,
     fontFamily: "Roboto-Regular",
     color: "#BDBDBD",
@@ -190,7 +237,6 @@ const styles = StyleSheet.create({
   },
   trashWrapper: {
     justifyContent: "center",
-    backgroundColor: "#F6F6F6",
     height: 40,
     width: 70,
     alignItems: "center",
